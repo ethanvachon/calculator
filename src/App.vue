4bc4c63d98f964b1250a0039ebd3d13be7af2444
Ethan Vachon <Ethan.Vachon@jacksons.com>
2023-08-16 11:59:29 -0600

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,147 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-auto">
+
+        <div class="card d-flex flex-column m-5">
+          <span class="secondary-screen text-muted">{{state.secondaryDisplay}} {{state.operator}}</span>  
+          <div class="screen-padding">
+            <input class="form-control screen" type="text" v-model="state.display">
+          </div>
+          <div class="d-flex button-area">
+            <div class="col-3">
+              <buttonTile @handleClick="clear()" :number="'C'" :color="'#a6a6a6'"></buttonTile>
+              <buttonTile @handleClick="handleNum('7')" :number="'7'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('4')" :number="'4'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('1')" :number="'1'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('0')" class="width" :number="'0'" :color="'#505050'"></buttonTile>
+            </div>
+            <div class="col-3">
+              <buttonTile :number="''" :color="'#a6a6a6'"></buttonTile>
+              <buttonTile @handleClick="handleNum('8')" :number="'8'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('5')" :number="'5'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('2')" :number="'2'" :color="'#505050'"></buttonTile>
+              <div></div>
+            </div>
+            <div class="col-3">
+              <buttonTile :number="''" :color="'#a6a6a6'"></buttonTile>
+              <buttonTile @handleClick="handleNum('9')" :number="'9'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('6')" :number="'6'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleNum('3')" :number="'3'" :color="'#505050'"></buttonTile>
+              <buttonTile @handleClick="handleDecimal()" :number="'.'" :color="'#505050'"></buttonTile>
+            </div>
+            <div class="col-3">
+              <buttonTile @handleClick="handleOperator('÷')" :number="'÷'" :color="'#ff8e00'"></buttonTile>
+              <buttonTile @handleClick="handleOperator('x')" :number="'x'" :color="'#ff8e00'"></buttonTile>
+              <buttonTile @handleClick="handleOperator('-')" :number="'-'" :color="'#ff8e00'"></buttonTile>
+              <buttonTile @handleClick="handleOperator('+')" :number="'+'" :color="'#ff8e00'"></buttonTile>
+              <buttonTile @handleClick="evaluate()" :number="'='" :color="'#ff8e00'"></buttonTile>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue'
+import buttonTile from '@/components/button-tile.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    buttonTile,
+  },
+  setup() {
+    const state = reactive({
+      display: '',
+      secondaryDisplay: '',
+      operator: null,
+    })
+    return {
+      state,
+      handleNum: function (num) {
+        state.display += num
+      },
+      clear: function () {
+        state.display = ''
+        state.secondaryDisplay = ''
+        state.operator = null
+      },
+      handleOperator: function (operator) {
+        if (state.operator == null) {
+          state.secondaryDisplay = state.display
+          state.display = ''
+        }
+        state.operator = operator
+      },
+      evaluate: function () {
+        state.display = this.determineResult()
+        state.secondaryDisplay = ''
+        state.operator = null
+      },
+      determineResult: function () {
+        switch(state.operator) {
+          case ('÷'):
+            return parseFloat(state.secondaryDisplay) / parseFloat(state.display)
+          case ('x'):
+            return parseFloat(state.secondaryDisplay) * parseFloat(state.display)
+          case ('-'):
+            return parseFloat(state.secondaryDisplay) - parseFloat(state.display)
+          case ('+'):
+            return parseFloat(state.secondaryDisplay) + parseFloat(state.display)
+        }
+      },
+      handleDecimal: function () {
+        if(!state.display.includes('.')) {
+          state.display += '.'
+        }
+      },
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.screen {
+  padding-bottom: 0px;
+  font-size: 2em;
+  font-weight: 500;
+  padding-top: 0.5em;
+}
+
+.secondary-screen {
+  position: relative;
+    overflow: visible;
+    height: 0px;
+    width: 0px;
+    top: 0.6em;
+    left: 1.3em;
+    white-space: nowrap;
+}
+
+.screen-padding {
+  padding: 0.5em;
+}
+
+.card {
+  background-color: black;
+  padding: 0.75em;
+  width: 21.5em;
+}
+
+.col-3 {
+  display: flex;
+  flex-direction: column;
+}
+
+.width {
+  width: 200%;
+}
+
+.button-area {
+  height: 25em;
 }
 </style>
---
+++ src/components/button-tile.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="button-tile">
+        <div @click="$emit('handleClick')" :style="backgroundColor" class="num-button">
+            <p class="m-0" :class="{'operator-sizing':backgroundColor.backgroundColor=='#ff8e00'}">{{state.number}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue'
+export default {
+    name: "buttonTile",
+    props: ['color', 'number'],
+    setup(props) {
+        const state = reactive({
+            number: props.number,
+        })
+        const backgroundColor = computed(() => {
+            return {
+                backgroundColor: `${props.color}`
+            }
+        })
+        return {
+            state,
+            backgroundColor,
+        }
+    }
+}
+</script>
+  
+<style scoped>
+.button-tile {
+    width: 5em;
+    height: 5em;
+    background-color: inherit;
+    padding: 0.25em;
+    z-index: 20;
+  }
+
+  .num-button {
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2em;
+    color: white;
+    cursor: pointer;
+  }
+
+  .num-button:hover {
+    filter: brightness(0.85);
+  }
+
+  .operator-sizing {
+    font-size: 1.4em;
+    padding-bottom: 0.2em;
+  }
+</style>
